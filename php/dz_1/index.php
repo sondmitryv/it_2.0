a230351825407e5fb4c085f0b7567c9eda31c4b3
Дмитрий Сон <sondmitryv@gmail.com>
2020-03-04 16:17:47 +0200

    first dz php

---
+++ php/dz_1/index.php
@@ -0,0 +1,87 @@
+<?php
+	/* task 1 */
+	function abrDo($text) {
+		$textItem = $text;
+		$outString = "";
+		echo $outString ;
+		for($i=1;  $i <= strlen($textItem); $i++){
+			if($textItem[$i] == ' ') {
+				$temp = $textItem[$i+1];
+				$outString .=  "$temp";
+			}
+		}
+		echo  mb_strtoupper( $textItem[0] . $outString);
+	};
+	abrDo('Donbass Machine Academy');
+?>
+<br>
+<?php
+	/* task 2 */
+	function truncate_string($str, $maxsymbol){
+		if(iconv_strlen($str) > $maxsymbol){
+			$temp = substr($str, 0, $maxsymbol);
+		}
+		echo $temp . '...';
+	}
+	truncate_string('lslsls', 5);
+	echo "<br>";
+?>
+
+<?php
+	/* task 3 */
+	function getCountSymbol($str, $sumbol){
+		$counter = 0;
+		for($i=0;  $i <= strlen($str); $i++){
+			if(mb_substr($str,$i,1) == $sumbol){
+				$counter++;
+			}
+		}
+		echo $counter;
+	}
+	getCountSymbol('Привет', 'и');
+	echo "<br>";
+?>
+
+<?php
+	/* task 4 */
+	if(isset($_POST)){
+		$str = $_POST;
+    	$str=trim($str);
+    	$str=strip_tags($str);
+    	$str=htmlspecialchars($str);
+    	echo $str;
+	}
+	
+	//processingDataFromForm("     sdadadadadad daaaa <div>dffsfs</div>    ");
+	echo "<br>";
+?>
+
+<?php
+	/* task 5 */
+	function getShortFio($fullFio){
+   		$fio=explode(" ",mb_strtolower($fullFio));
+   		$surname=mb_strtoupper( mb_substr($fio[0], 0, 1)). mb_strtolower( mb_substr($fio[0], 1,mb_strlen($fio[0])-1));
+   		$name=mb_strtoupper( mb_substr($fio[1], 0, 1)).".";
+   		$patronymic=mb_strtoupper( mb_substr($fio[2], 0, 1)).".";
+
+   		echo "${surname} ${name} ${patronymic}";
+	}
+	getShortFio("Сон Дмитрий Владимирович");
+	echo "<br>";
+?>
+
+<?php
+	/* task 6 */
+	function getFileExtension($fileName){
+		$sumbol = '.';
+		for($i=0;  $i <= strlen($fileName); $i++){
+			if($fileName[$i] == $sumbol){
+				echo  mb_substr($fileName,($i+1),strlen($fileName));
+			}
+		}
+	}
+	getFileExtension("myfile.docx")
+?>
+
+
+	
